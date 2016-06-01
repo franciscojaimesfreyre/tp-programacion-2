5d917ca3ed727dbb89addeaa166b565740cd7b8e
franciscojaimesfreyre <franciscojaimesfreyre@gmail.com>
2016-06-01 16:19:14 -0300

    creoConsumo

---
+++ EstadoConsumo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Francisco on 01/06/16.
+//
+
+#include "EstadoConsumo.h"
---
+++ EstadoConsumo.h
@@ -0,0 +1,24 @@
+//
+// Created by Francisco on 01/06/16.
+//
+
+#ifndef TRABAJODECLUA_ESTADOCONSUMO_H
+#define TRABAJODECLUA_ESTADOCONSUMO_H
+
+#include <iostream>
+using namespace std;
+
+
+class EstadoConsumo {
+
+private:
+    string numeroCliente;
+    string consumo;
+
+public:
+
+
+};
+
+
+#endif //TRABAJODECLUA_ESTADOCONSUMO_H
