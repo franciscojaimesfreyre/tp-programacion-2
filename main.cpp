879841b04e0a686a9bfef35fdfa76f41fd62aa07
MarianoFacundo <mfacundo94@gmail.com>
2016-06-02 15:14:24 -0300

    modifcaaa

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-
+    Servicio servicio;
     Cliente cliente ;
     string s;
     int n;
@@ -12,8 +12,37 @@ int main() {
     while(getline(entra,s)){
         istringstream iss(s);
         iss>>cliente;
-        cout<<"Cliente: "<<cliente<<endl;
+        int ultimo = servicio.insertarCliente(cliente);
+
+        cout<<"Cliente: "<<servicio.getListaClientes()[ultimo]<<endl;
+
+
     }
+
+
+    EstadoConsumo consumosCliente[5];
+    int estActual = 0;
+
+    EstadoConsumo temp;
+    string ss;
+    ifstream entra2("consumos.txt");
+    while(getline(entra2,ss)){
+        istringstream iss(ss);
+        iss>>temp;
+        cout<<temp.getNumeroCliente();
+
+
+        /*
+        int ultimo = servicio.insertarCliente(cliente);
+
+        cout<<"Cliente: "<<servicio.getListaClientes()[ultimo]<<endl;
+         */
+
+
+    }
+
+
+
 
     return 0;
 }
